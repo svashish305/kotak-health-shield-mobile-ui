f636b8d2680e37ee09563fb1ba65b92097c79ce9
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-07 15:44:44 +0530

    code refactor esp. font-size styles

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { DetailsComponent } from './components/details/details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', component: LandingPageComponent, },
+  { path: 'details', component: DetailsComponent, },
+  { path: '**', redirectTo: '/landing' },
 ];
 
 @NgModule({
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
     LandingPageComponent,
+    DetailsComponent,
   ],
   imports: [
     CommonModule,
