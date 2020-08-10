c88ccb6c115416b1361c516a469663f31872d90c
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-10 13:03:58 +0530

    completed details screen

--- src/app/modules/home/components/details/details.component.ts
+++ src/app/modules/home/components/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DateValidator } from '../../../../shared/date-validator.validator';
+import { SearchCountryField, TooltipLabel, CountryISO } from 'ngx-intl-tel-input';
 
 @Component({
   selector: 'app-details',
@@ -15,6 +16,17 @@ export class DetailsComponent implements OnInit {
   showMinor = false;
   minDate = { year: 1920, month: 1, day: 1 };
   age = 0;
+  incomes = [
+    { label: '1-5', value: '1-5' },
+    { label: '5-10', value: '5-10' },
+    { label: '10-15', value: '10-15' },
+    { label: '15-20', value: '15-20' },
+  ];
+  separateDialCode = true;
+  SearchCountryField = SearchCountryField;
+  TooltipLabel = TooltipLabel;
+  CountryISO = CountryISO;
+  preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
 
   constructor(private fb: FormBuilder, private router: Router) { }
 
@@ -22,9 +34,14 @@ export class DetailsComponent implements OnInit {
     this.detailsForm = this.fb.group({
       name: ['Rajesh Wadhwa', [Validators.required]],
       birthdate: ['', Validators.compose([Validators.required, DateValidator.dateValidator])],
-      mobileNo: ['', Validators.required],
+      income: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
       emailId: new FormControl('rajesh.wadhwa@gmail.com', [Validators.required, Validators.email]) 
     });
+  }
+
+  assignDropdownVal(field, val) {
+    this.detailsForm.controls[field].setValue(val);
   }
 
   ageCalc() {
@@ -34,6 +51,10 @@ export class DetailsComponent implements OnInit {
     this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365.25);
     this.showMinor = this.age < 18;
     return this.age;
+  }
+
+  changePreferredCountries() {
+    this.preferredCountries = [CountryISO.India, CountryISO.Canada];
   }
 
   goToDetailsTwo() {
