a5f5a8bdae663cf83c607df2c90699a2ea3f8592
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-11 16:26:24 +0530

    minor style changes

--- src/app/modules/home/components/details-two/details-two.component.ts
+++ src/app/modules/home/components/details-two/details-two.component.ts
@@ -16,25 +16,29 @@ export class DetailsTwoComponent implements OnInit {
       id: 1,
       title: 'Cancer Shield',
       desc: 'Average cost of cancer can go upto 20 lacs',
-      duration: 0
+      duration: 0,
+      perDayCharge: 5
     },
     {
       id: 2,
       title: 'Cardiac Shield',
       desc: '4 out of 10 people face cardiac issues',
-      duration: 0
+      duration: 0,
+      perDayCharge: 5
     },
     {
       id: 3,
       title: 'Livo Shield',
       desc: 'End stage liver disease can cause death',
-      duration: 0
+      duration: 0,
+      perDayCharge: 7
     },
     {
       id: 4,
       title: 'Neuro Shield',
       desc: '1 in 6 people living with one or more Neurological condition',
-      duration: 0
+      duration: 0,
+      perDayCharge: 7
     }
   ];
   annualValues = ["Annually", "Monthly"];
