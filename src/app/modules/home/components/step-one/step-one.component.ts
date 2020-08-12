7bedabde43bc86466ea07d75c524de1b6ac042b2
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-12 16:09:49 +0530

    added step-one form 1

--- src/app/modules/home/components/step-one/step-one.component.ts
+++ src/app/modules/home/components/step-one/step-one.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-step-one',
@@ -11,12 +11,41 @@ export class StepOneComponent implements OnInit {
   @Input() healthTabRef;
   formSubmitted = false;
   personalDetailsForm: FormGroup;
+  resiStatuses = [
+    { label: 'Resident Individual', value: 'Resident Individual' },
+    { label: 'Non-Ordinarily Resident', value: 'Non-Ordinarily Resident' },
+    { label: 'Non-Resident', value: 'Non-Resident' },
+  ];
+  maritalStatuses = [
+    { label: 'Unmarried', value: 'Unmarried' },
+    { label: 'Married', value: 'Married' },
+    { label: 'Divorced', value: 'Divorced' },
+    { label: 'Widow', value: 'Widow' }
+  ];
+  eduStatuses = [
+    { label: 'Associate', value: 'Associate' },
+    { label: `Bachelor's`, value: `Bachelor's` },
+    { label: `Master's`, value: `Master's` },
+    { label: 'Doctorate', value: 'Doctorate' }
+  ];
+  occuStatuses = [
+    { label: 'Unemployed', value: 'Unemployed' },
+    { label: 'Salaried (Public)', value: 'Salaried (Public)' },
+    { label: 'Salaried (Private)', value: 'Salaried (Private)' },
+    { label: 'Own Business', value: 'Own Business' }
+  ];
 
   constructor(private location: Location, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.personalDetailsForm = this.fb.group({
-      
+      residentialStatus: ['Resident Individual', Validators.required],
+      currentPin: ['400 607', Validators.required],
+      mothersName: ['Sushmita', Validators.required],
+      maritalStatus: ['Married', Validators.required],
+      education: [`Bachelor's Degree`, Validators.required],
+      occupation: [`Salaried (Private)`, Validators.required],
+      panNo: ['AWPRGH9378', Validators.required]
     });
   }
 
