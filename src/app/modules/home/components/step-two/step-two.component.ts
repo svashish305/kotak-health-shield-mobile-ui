d84357b0512b283214587ff3db6212e83fdb7dbd
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-13 11:51:41 +0530

    WIP step-2 part-1

--- src/app/modules/home/components/step-two/step-two.component.ts
+++ src/app/modules/home/components/step-two/step-two.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-step-two',
@@ -7,20 +8,48 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class StepTwoComponent implements OnInit {
   @Input() healthTabRef;
+  firstFormSubmitted = false;
   formSubmitted = false;
+  criticalIllnessForm: FormGroup;
+  covidQAndAForm: FormGroup;
+  personalHealthDetailsForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.criticalIllnessForm = this.fb.group({
+      ciCover: ['No'],
+      ciClaim: ['No'],
+      ciDeclined: ['No'],
+      insuranceUpdated: ['No']
+    });
+
+    this.covidQAndAForm = this.fb.group({
+      goodHealth: ['No'],
+      symptoms: ['No'],
+      travelledAbroad: ['No'],
+      willTravel: ['No'],
+      quarantined: ['No'],
+      covidPositive: ['No']
+    });
   }
 
   navigateBack() {
-    this.healthTabRef.select('1');
+    if(!this.firstFormSubmitted) {
+      this.healthTabRef.select('1');
+    } else {
+      this.firstFormSubmitted = false;
+    }
   }
 
   navigate() {
-    this.formSubmitted = true;
-    this.healthTabRef.select('3');
+    if(!this.firstFormSubmitted) {
+      this.firstFormSubmitted = true;
+      this.formSubmitted = false;
+    } else {
+      this.formSubmitted = true;
+      this.healthTabRef.select('3');
+    }
   }
 
 }
