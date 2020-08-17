959154a85621de49c0e1b0d452d25a932b2b6004
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-17 12:27:10 +0530

    added aadhaar otp page

--- src/app/modules/home/components/aadhaar-details/aadhaar-details.component.ts
+++ src/app/modules/home/components/aadhaar-details/aadhaar-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aadhaar-details',
@@ -8,9 +10,31 @@ import { Location } from '@angular/common';
 })
 export class AadhaarDetailsComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  aadhaarForm: FormGroup;
+  authorize = false;
+  otpGenerated = false;
+  otpForm: FormGroup;
+  formSubmitted = false;
+
+  constructor(private location: Location, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.aadhaarForm = this.fb.group({
+      aadhaarNo: ['', Validators.required],
+      authorize: ['', Validators.required],
+    });
+
+    this.otpForm = this.fb.group({
+      otp: new FormControl('400607', [Validators.required, Validators.pattern('[0-9]{6}')]),
+    });
+  }
+
+  generateOTP() {
+    this.otpGenerated = true;
+  }
+
+  resendOtp() {
+    
   }
 
   navigateBack() {
