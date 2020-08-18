cbb5eb6b614f32190e30895335be53d5e492f3cb
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-18 15:30:25 +0530

    reviewscreen WIP

--- src/app/modules/home/components/review-details/review-details.component.ts
+++ src/app/modules/home/components/review-details/review-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-review-details',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewDetailsComponent implements OnInit {
 
-  constructor() { }
+  reviewForm: FormGroup;
+
+  constructor(
+    private location: Location, 
+    private router: Router,
+    private fb: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.reviewForm = this.fb.group({
+      
+    });
+  }
+
+  navigateBack() {
+    this.location.back();
+  }
+
+  navigate() {
+
   }
 
 }
