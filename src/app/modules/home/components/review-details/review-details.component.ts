99de1c62bb94a2382df5fc2a6d2ea9c510111cf2
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-18 16:42:18 +0530

    added modal in review-screen

--- src/app/modules/home/components/review-details/review-details.component.ts
+++ src/app/modules/home/components/review-details/review-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-review-details',
@@ -10,18 +11,47 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class ReviewDetailsComponent implements OnInit {
 
+  formSubmitted = false;
   reviewForm: FormGroup;
+  proposalFormAccepted = false;
+  fatcaFormAccepted = false;
+  covidDecAccepted = false;
+  opened = false;
+  closeResult: string;
 
   constructor(
     private location: Location, 
     private router: Router,
+    private modalService: NgbModal, 
     private fb: FormBuilder
     ) { }
 
   ngOnInit(): void {
     this.reviewForm = this.fb.group({
-      
     });
+  }
+
+  open(content) {
+    this.opened = true;
+    this.modalService.open(content, {
+      ariaLabelledBy: 'modal-basic-title',
+      centered: true,
+      backdropClass: 'dark-backdrop'
+    }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  openConditions(event, content) {
+    // if (!this.opened) {
+    //   this.opened = true;
+    //   this.open(content);
+    // }
+    if (event.target.checked) {
+      this.open(content);
+    }
   }
 
   navigateBack() {
@@ -29,7 +59,7 @@ export class ReviewDetailsComponent implements OnInit {
   }
 
   navigate() {
-
+    this.formSubmitted = true;
   }
 
 }
